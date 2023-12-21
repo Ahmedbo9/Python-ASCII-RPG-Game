b330d9b1f669c9c7361054ab15be28994c1b65bc
Ahmed Ben Othman <ahmed-ben-othman@Ahmeds-MacBook-Air.local>
2023-12-21 01:17:19 -0500

    menu is  working fine

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 
 run = True
 menu = True
@@ -6,6 +7,14 @@ rules = False
 game = True
 HP = 50
 ATK = 5
+
+
+def clear():
+    os.system('clear')
+
+
+def draw():
+    print("_______________________________________________________")
 
 
 def save():
@@ -23,11 +32,12 @@ def save():
 
 while run:
     while menu:
+        clear()
         print("Welcome to my game , please choose an option from 1 to 4")
         print("1 | NEW GAME")
         print("2 | LOAD SAVED GAME")
-        print("3 |  RULES")
-        print("4 |  QUIT GAME")
+        print("3 | RULES")
+        print("4 | QUIT GAME")
         if rules:
             print("rules")
             rules = False
@@ -36,19 +46,36 @@ while run:
         else:
             playerChoice = input("# ")
         if playerChoice == "1":
+            clear()
             playerName = input("Enter your name padawan: ")
             menu = False
             play = True
         elif playerChoice == "2":
-            pass
+            f = open("save.txt", "r")
+            load_save = f.readlines()
+            playerName = load_save[0][:-1]
+            HP = load_save[1][:-1]
+            ATK = load_save[2][:-1]
+            f.close()
+            clear()
+            print("Save is Loaded , welcome back " + playerName)
+            print(playerName, HP, ATK)
+            input("> ")
+            menu = False
+            play = True
+
         elif playerChoice == "3":
             rules = True
         elif playerChoice == "4":
             quit()
 
     while play:
+        clear()
+        draw()
+        print("Type '0' to save and exit the game")
+        draw()
         save()
-        print("Welcome  padawan " + playerName)
+        print("IN PLAY " + playerName)
         destination = input("# ")
         if destination == "0":
             play = False
