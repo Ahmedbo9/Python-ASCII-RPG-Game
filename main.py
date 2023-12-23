9f0fb3f767a6cb4e50242ce163fc38c4b555f535
Ahmed Ben Othman <ahmed-ben-othman@Ahmeds-MacBook-Air.local>
2023-12-22 22:50:48 -0500

    adding combat systeme and gui for health bars

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 run = True
 menu = True
@@ -6,6 +7,8 @@ play = False
 rules = False
 game = True
 key = False
+fight = False
+immune = True
 HP = 50
 HPMAX = HP
 ATK = 5
@@ -14,6 +17,7 @@ elixir = 0
 gold = 0
 x = 0
 y = 0
+
 #  x = 0       x = 1       x = 2       x = 3       x = 4       x = 5         x = 6
 gameMap = [["plains", "plains", "plains", "plains", "forest", "mountain", "cave"],  # y = 0
            ["forest", "forest", "forest", "forest", "forest", "hills", "mountain"],  # y = 1
@@ -64,22 +68,22 @@ mobs = {
     "Goblin": {
         "hp": 15,
         "at": 3,
-        "go": 8
+        "gold": 8
     },
     "Orc": {
         "hp": 35,
         "at": 5,
-        "go": 18
+        "gold": 18
     },
     "Slime": {
         "hp": 30,
         "at": 2,
-        "go": 12
+        "gold": 12
     },
     "Dragon": {
         "hp": 100,
         "at": 8,
-        "go": 100
+        "gold": 100
     }
 }
 
@@ -89,7 +93,7 @@ def print_player_info():
     print("LOCATION: " + biom[gameMap[y][x]]["t"])
     draw()
     print("NAME: " + playerName)
-    print("HP: " + str(HP) + "/" + str(HPMAX))
+    print(playerName + ' health : ' + show_health_bar(HP, HPMAX))
     print("ATK: " + str(ATK))
     print("POTIONS: " + str(potion))
     print("ELIXIRS: " + str(elixir))
@@ -123,6 +127,31 @@ def save():
     for stat in stats:
         file.write(stat + "\n")
     file.close()
+
+
+def show_health_bar(current_hp, max_hp):
+    bars = 20
+    remaining_health_symbol = "█"
+    lost_health_symbol = "_"
+    color_green = "\033[92m"
+    color_yellow = "\33[33m"
+    color_red = "\033[91m"
+    color_default = "\033[0m"
+
+    remaining_health_bars = round(current_hp / max_hp * bars)
+    lost_health_bars = bars - remaining_health_bars
+
+    if current_hp > 0.66 * max_hp:
+        health_color = color_green
+    elif current_hp > 0.33 * max_hp:
+        health_color = color_yellow
+    else:
+        health_color = color_red
+
+    health_bar = f"|{health_color}{remaining_health_bars * remaining_health_symbol}" \
+                 f"{lost_health_bars * lost_health_symbol}{color_default}|"
+
+    return health_bar
 
 
 while run:
@@ -175,8 +204,110 @@ while run:
         elif playerChoice == "4":
             quit()
 
+
+    def heal(heal_amount):
+        pass
+
+
+    def battle():
+        global fight, run, play, HP, ATK, potion, gold, elixir
+        print(playerName + " you need to fight")
+        enemy = random.choice(e_list)
+        enemy_hp = mobs[enemy]["hp"]
+        enemy_atk = mobs[enemy]["at"]
+        enemy_gold = mobs[enemy]["gold"]
+        enemy_hpmax = enemy_hp
+        while fight:
+            clear()
+            draw()
+            print("Defeat the " + enemy + "!")
+            draw()
+            print(enemy + ' health :' + show_health_bar(enemy_hp, enemy_hpmax))
+            print(playerName + ' health :' + show_health_bar(HP, HPMAX))
+            print("POTIONS: " + str(potion))
+            print("ELIXIR: " + str(elixir))
+            draw()
+            print("1 - ATTACK")
+            if potion > 0:
+                print("2 - USE POTION (30HP)")
+            if elixir > 0:
+                print("3 - USE ELIXIR (50HP)")
+            draw()
+
+            choice = input("# ")
+
+            if choice == "1":
+                enemy_hp -= ATK
+                print(playerName + " dealt " + str(ATK) + " damage to the " + enemy + ".")
+                print(enemy + ' health :' + show_health_bar(enemy_hp, enemy_hpmax))
+                if enemy_hp > 0:
+                    HP -= enemy_atk
+                    print(enemy + " dealt " + str(enemy_atk) + " damage to " + playerName + ".")
+                    print(playerName + ' health : ' + show_health_bar(HP, HPMAX))
+
+                input("> ")
+
+            elif choice == "2":
+                if potion > 0:
+                    potion -= 1
+                    heal(30)
+                    HP -= enemy_atk
+                    print(enemy + " dealt " + str(enemy_atk) + " damage to " + playerName + ".")
+                    print(playerName + ' health : ' + show_health_bar(HP, HPMAX))
+
+                else:
+                    print("No potions!")
+                input("> ")
+
+            elif choice == "3":
+                if elixir > 0:
+                    elixir -= 1
+                    heal(50)
+                    HP -= enemy_atk
+                    print(enemy + " dealt " + str(enemy_atk) + " damage to " + playerName + ".")
+                    print(playerName + ' health : ' + show_health_bar(HP, HPMAX))
+
+                else:
+                    print("No elixirs!")
+                input("> ")
+
+            if HP <= 0:
+                print(enemy + " defeated " + playerName + "...")
+                draw()
+                fight = False
+                play = False
+                run = False
+                print("GAME OVER")
+                input("> ")
+
+            if enemy_hp <= 0:
+                print(playerName + " defeated the " + enemy + "!")
+                draw()
+                fight = False
+                gold += enemy_gold
+                print("You've found " + str(enemy_gold) + " gold!")
+                if random.randint(0, 100) < 30:
+                    potion += 1
+                    print("You've found a potion!")
+                if enemy == "Dragon":
+                    draw()
+                    print("Congratulations, you've finished the game!")
+                    boss = False
+                    play = False
+                    run = False
+                input("> ")
+                clear()
+
+
     while play:
+        clear()
         save()
+        if not immune:
+            if biom[gameMap[y][x]]["e"]:
+                if random.random() < 0.6:
+                    fight = True
+                    battle()
+
         print_player_info()
         if y > 0:
             print("1 - NORTH")
@@ -187,7 +318,7 @@ while run:
         if x > 0:
             print("4 - WEST")
 
-        print('PLAYER LOCATION', x ,y )
+        print('PLAYER LOCATION', x, y)
         draw()
 
         print("Type '0' to save and exit the game")
@@ -199,13 +330,18 @@ while run:
         elif destination == "1":
             if y > 0:
                 y -= 1
+                immune = False
         elif destination == "2":
             if x < x_len:
                 x += 1
+                immune = False
+
         elif destination == "3":
             if y < y_len:
                 y += 1
+                immune = False
+
         elif destination == "4":
             if x > 0:
                 x -= 1
-
+                immune = False
